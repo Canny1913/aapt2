2584168a44a4b107785f8b7b23430458b3cb27cf
Canny <94744045+Canny1913@users.noreply.github.com>
2024-03-27 01:31:02 +0300

    disable debug

--- build.sh
+++ build.sh
@@ -83,7 +83,7 @@ cmake -GNinja \
 -DCMAKE_C_COMPILER="$compiler_bin_directory$c_compiler" \
 -DCMAKE_CXX_COMPILER="$compiler_bin_directory$cxx_compiler" \
 -DCMAKE_BUILD_WITH_INSTALL_RPATH=True \
--DCMAKE_BUILD_TYPE=Release \
+-DCMAKE_BUILD_TYPE=Debug \
 -DANDROID_ABI="$architecture" \
 -DTARGET_ABI="${target_abi[$architecture]}" \
 -DPROTOC_PATH="/usr/local/bin/protoc" \
@@ -91,8 +91,6 @@ cmake -GNinja \
 .. || exit 1
 
 ninja || exit 1
-
-"$NDK_TOOLCHAIN/bin/llvm-strip" --strip-unneeded  "$aapt_binary_path"
 
 # Create bin directory.
 mkdir -p "$bin_directory"
